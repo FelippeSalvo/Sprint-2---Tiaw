91af5bcdbc9d91ef68804a49005f53018fe1f943
FelippeSalvo <Felippesalvo7@gmail.com>
2025-06-01 18:50:34 -0300

    iniciando o ambiente de trabalho

---
+++ TIAW - Entrega Sprint 2 FelippeSalvo/public/Assets/js/app.js
@@ -0,0 +1,31 @@
+const API = "http://localhost:3000/usuarios";
+
+
+document.querySelector(".login_btn")?.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const email = document.querySelector('input[placeholder="Email Institucional"]').value;
+    const senha = document.querySelector('input[placeholder="Senha"]').value;
+    const tipoConta = document.getElementById("tipoContaLogin")?.value;
+
+    if (!email || !senha || !tipoConta) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    if (!email.endsWith("@sga.pucminas.br")) {
+        alert("Use um email institucional (@sga.pucminas.br)");
+        return;
+    }
+
+    fetch(`http://localhost:3000/usuarios?email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}&tipoConta=${encodeURIComponent(tipoConta)}`)
+        .then(res => res.json())
+        .then(usuarios => {
+            if (usuarios.length > 0) {
+                alert("Login bem-sucedido!");
+                window.location.href = "placeholder.html";
+            } else {
+                alert("Credenciais incorretas!");
+            }
+        });
+});
