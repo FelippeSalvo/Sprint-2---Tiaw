22f89de728da5d2b28dc483b075409d26ab2e9dc
FelippeSalvo <Felippesalvo7@gmail.com>
2025-06-01 21:32:29 -0300

    ultimo teste

--- TIAW - Entrega Sprint 2 FelippeSalvo/public/Assets/js/app.js
+++ TIAW - Entrega Sprint 2 FelippeSalvo/public/Assets/js/app.js
@@ -1,4 +1,11 @@
+
 const API = "https://json-server-olx-puc.onrender.com/usuarios";
+
+
+function gerarId() {
+    return Math.random().toString(36).substr(2, 4);
+}
+
 
 document.querySelector(".login_btn")?.addEventListener("click", function (event) {
     event.preventDefault();
@@ -17,6 +24,7 @@ document.querySelector(".login_btn")?.addEventListener("click", function (event)
         return;
     }
 
+   
     fetch(`${API}?email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}&tipoConta=${encodeURIComponent(tipoConta)}`)
         .then(res => res.json())
         .then(usuarios => {
@@ -24,10 +32,61 @@ document.querySelector(".login_btn")?.addEventListener("click", function (event)
                 alert("Login bem-sucedido!");
                 window.location.href = "placeholder.html"; 
             } else {
-                alert("Credenciais incorretas! Verifique o email, senha e tipo de conta.");
+                alert("Credenciais incorretas! Verifique os dados informados.");
             }
         })
         .catch(() => {
             alert("Erro ao conectar com o servidor.");
+        });
+});
+
+
+document.getElementById("botaoCadastro")?.addEventListener("click", function () {
+    const email = document.getElementById("email")?.value.trim();
+    const nomeUsuario = document.getElementById("usuario")?.value.trim();
+    const senha = document.getElementById("senha")?.value.trim();
+    const confirmarSenha = document.getElementById("confirmarSenha")?.value.trim();
+    const tipoConta = document.getElementById("tipoConta")?.value;
+
+    if (!email || !nomeUsuario || !senha || !confirmarSenha || !tipoConta) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    if (!email.endsWith("@sga.pucminas.br")) {
+        alert("Use um email institucional (@sga.pucminas.br)");
+        return;
+    }
+
+    if (senha !== confirmarSenha) {
+        alert("As senhas não coincidem!");
+        return;
+    }
+
+    
+        const novoUsuario = {
+        id: gerarId(),
+        email,
+        senha,
+        tipoConta,
+        nomeUsuario
+    };
+
+    
+    fetch(API, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(novoUsuario)
+    })
+        .then(res => {
+            if (res.ok) {
+                alert("Conta criada com sucesso!");
+                window.location.href = "index.html"; 
+            } else {
+                alert("Erro ao cadastrar. Tente novamente.");
+            }
+        })
+        .catch(() => {
+            alert("Erro ao se conectar com o servidor.");
         });
 });
