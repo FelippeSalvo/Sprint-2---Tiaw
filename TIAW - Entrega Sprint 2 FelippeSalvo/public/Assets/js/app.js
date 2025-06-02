1cba4cc3cc9f69fca7a4d649a0d949d7aa04f9d0
FelippeSalvo <Felippesalvo7@gmail.com>
2025-06-01 21:18:29 -0300

    .

--- TIAW - Entrega Sprint 2 FelippeSalvo/public/Assets/js/app.js
+++ TIAW - Entrega Sprint 2 FelippeSalvo/public/Assets/js/app.js
@@ -1,14 +1,19 @@
 const API = "https://json-server-olx-puc.onrender.com/usuarios";
 
 
-document.querySelector(".login_btn")?.addEventListener("click", function (event) {
-    event.preventDefault();
+function gerarId() {
+    return Math.random().toString(36).substr(2, 4);
+}
 
-    const email = document.querySelector('input[placeholder="Email Institucional"]').value;
-    const senha = document.querySelector('input[placeholder="Senha"]').value;
-    const tipoConta = document.getElementById("tipoContaLogin")?.value;
 
-    if (!email || !senha || !tipoConta) {
+document.getElementById("botaoCadastro")?.addEventListener("click", function () {
+    const email = document.getElementById("email").value;
+    const nomeUsuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+    const confirmarSenha = document.getElementById("confirmarSenha").value;
+    const tipoConta = document.getElementById("tipoConta").value;
+
+    if (!email || !nomeUsuario || !senha || !confirmarSenha || !tipoConta) {
         alert("Preencha todos os campos!");
         return;
     }
@@ -18,14 +23,33 @@ document.querySelector(".login_btn")?.addEventListener("click", function (event)
         return;
     }
 
-    fetch(`https://json-server-olx-puc.onrender.com/usuarios?email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}&tipoConta=${encodeURIComponent(tipoConta)}`)
-        .then(res => res.json())
-        .then(usuarios => {
-            if (usuarios.length > 0) {
-                alert("Login bem-sucedido!");
-                window.location.href = "placeholder.html";
-            } else {
-                alert("Credenciais incorretas!");
-            }
-        });
+    if (senha !== confirmarSenha) {
+        alert("As senhas não coincidem!");
+        return;
+    }
+
+    const novoUsuario = {
+        id: gerarId(),
+        email,
+        senha,
+        tipoConta,
+        nomeUsuario
+    };
+
+    fetch(API, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(novoUsuario)
+    })
+    .then(res => {
+        if (res.ok) {
+            alert("Conta criada com sucesso!");
+            window.location.href = "index.html";
+        } else {
+            alert("Erro ao cadastrar. Tente novamente.");
+        }
+    })
+    .catch(() => {
+        alert("Erro ao se conectar com o servidor.");
+    });
 });
